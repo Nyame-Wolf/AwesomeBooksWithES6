67871298b59ab3bad758a2dd17952259955f1c78
Nyame-Wolf <mumenyam@gmail.com>
2022-07-04 23:20:40 +0300

    Refactor aweoseme books to use modules. Add luxon.js external module to display time.

---
+++ modules/navVariables.js
@@ -0,0 +1,10 @@
+const addNewAuthorLink = document.querySelector('.form-link');
+const listLink = document.querySelector('.list-link');
+const contactLink = document.querySelector('.contact-link');
+const addNewAuthor = document.getElementById('form');
+const list = document.getElementById('list');
+const contact = document.getElementById('contact');
+
+export {
+  addNewAuthorLink, listLink, contactLink, addNewAuthor, list, contact,
+};
